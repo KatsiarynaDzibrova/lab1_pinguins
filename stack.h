10be52777cf58bd847e87626a89f386ede82cf2a
Dzibrova Katsiaryna <dzibrovak@famcs-steps.yaconnect.com>
2019-09-13 13:29:26 +0300

    devided stack into .h and .cpp

--- stack.h
+++ stack.h
@@ -6,69 +6,23 @@ using namespace std;
 
 class Stack {
 public:
-  explicit Stack(int max_size = 100) :
-    max_size_(max_size), tail_(0) {
-    int* copy_ = new int[max_size_];
-    data_ = copy_;
-  }
+  explicit Stack(int max_size = 100);
 
-  int Size() const {
-    return tail_;
-  }
+  int Size() const;
 
-  bool IsEmpty() const {
-    return tail_ == 0;
-  }
+  bool IsEmpty() const;
 
-  bool IsFull() const {
-    return tail_ == max_size_;
-  }
+  bool IsFull() const;
 
-  void Resize(int new_size_) {
-    int* big_copy_ = new int[new_size_];
-    for (int i = 0; i < fmin(tail_, new_size_); i++) {
-      big_copy_[i] = data_[i];
-    }
-    delete[] data_;
-    data_ = big_copy_;
-    max_size_ = new_size_;
-  }
+  void Resize(int new_size_);
 
-  bool PushBack(int value) {
-    if (IsFull()) {
-      Resize(2 * max_size_);
-    }
-    data_[tail_] = value;
-    tail_++;
-    return true;
-  }
+  bool PushBack(int value);
 
-  bool PopBack() {
-    if (IsEmpty()) {
-      return false;
-    }
-    tail_--;
-    if (tail_ < (max_size_ * 0.5)) {
-      Resize(max_size_ * 0.5);
-    }
-    return true;
-  }
+  bool PopBack();
 
-  bool operator==(const Stack& other) const {
-    if (tail_ != other.Size()) {
-      return false;
-    }
-    for (int i = 0; i < other.Size(); i++) {
-      if (data_[i] != other.data_[i]) {
-        return false;
-      };
-    }
-    return true;
-  }
+  bool operator==(const Stack& other) const;
 
-  ~ Stack() {
-    delete[] data_;
-  }
+  ~ Stack();
 
 private:
   int* data_;

99013b41979a06d3cc2a8af98bbebba789fcef07
yaninasan10 <yaninasan@tut.by>
2019-09-19 20:35:44 +0300

    fixes

--- abstractview.h
+++ abstractview.h
@@ -1,6 +1,17 @@
 #pragma once
+#include <iostream>
+#include "framework.h"
+
 class  AbstractView {
 public:
-	virtual void on_pushButton_clicked() = 0;
 	virtual ~AbstractView() = default;
+
+	virtual void NewPinguinPressed() = 0;
+	virtual void DeletePinguinPressed() = 0;
+	virtual void IfEqualPressed() = 0;
+	virtual void ChangeSize() = 0;
+	virtual void SetIfEqual(std::string tr) = 0;
+	virtual void setDialog(HWND dlg) = 0;
+	virtual void ShowPinguin(std::string s) = 0;
+	virtual void HidePinguin() = 0;
 };
